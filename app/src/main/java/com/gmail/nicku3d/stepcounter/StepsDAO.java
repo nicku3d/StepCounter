637c73044d0987e4521e9eca780671237f5c0996
nicku3d <nicku3d@gmail.com>
2020-02-07 13:20:41 +0100

    Added StepsDAO interface

---
+++ app/src/main/java/com/gmail/nicku3d/stepcounter/StepsDAO.java
@@ -0,0 +1,20 @@
+package com.gmail.nicku3d.stepcounter;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface StepsDAO {
+
+    @Insert
+    void insert(DailySteps dailySteps);
+
+    @Query("DELETE FROM steps_table")
+    void deleteAll();
+
+    @Query("SELECT * from steps_table ORDER BY date")
+    List<DailySteps> getAllDailySteps();
+}
